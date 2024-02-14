f6a6c678e86a699ef0be1d9cfdd7ee82a9ef4129
Rhayar Mascarello <rhayar.mascarello@gmail.com>
2024-02-13 22:27:23 -0300

    commit

--- app/app.py
+++ app/app.py
@@ -15,6 +15,8 @@ def init_db():
         cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS chat_history
                           (number TEXT, content TEXT, msg_type TEXT)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS numeros_desabilitados
+                          (number TEXT)''')
 
 def add_message_to_db(number, content, msg_type):
     with sqlite3.connect(DATABASE) as conn:
@@ -27,6 +29,27 @@ def get_messages_from_db(number):
         cursor = conn.cursor()
         cursor.execute("SELECT content, msg_type FROM chat_history WHERE number=?", (number,))
         return cursor.fetchall()
+    
+def check_number_disabled(number):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT number FROM numeros_desabilitados WHERE number=?", (number,))
+        return cursor.fetchone() is not None
+
+def disable_number(number):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO numeros_desabilitados (number) VALUES (?)", (number,))
+
+@app.route('/disable-number', methods=['POST'])
+def post_disable_number():
+    data = request.get_json()
+    number = data.get('number', '')
+    if number:
+        disable_number(number)
+        return jsonify({"message": "Número desabilitado com sucesso."}), 200
+    else:
+        return jsonify({"error": "Número não fornecido."}), 400
 
 @app.route('/bot', methods=['POST'])
 def bot_response():
@@ -37,34 +60,32 @@ def bot_response():
     number = request.form.get('From', '')
     message = request.form.get('Body', '')
 
+    if check_number_disabled(number):
+        return '', 204  # Número desabilitado, retorna 204 No Content
+
     add_message_to_db(number, message, "human")
 
     # Logic to fetch previous chat history, run the model, and generate response
     prev_messages = get_messages_from_db(number)
     chat_history = "\n".join([msg[0] for msg in prev_messages])
 
-    template = f"""Seu nome é Antonella e você é um assitente de vendas e suporte da VirtualGenius Tech, sua função é auxiliar clientes em potencial e existentes a compreender melhor os produtos e serviços que oferecemos, bem como responder a quaisquer dúvidas que possam surgir sobre nossos planos e soluções. Aqui estão algumas diretrizes sobre como você deve proceder:
-Vendas e Promoção:
-Destaque os benefícios e recursos exclusivos de nossos serviços, enfatizando como nossas soluções de IA podem melhorar a eficiência e a tomada de decisões nos negócios dos clientes.
-Esteja pronto para comparar nossos serviços com concorrentes, mostrando o que nos torna a melhor escolha.
-Suporte Técnico e Dúvidas:
-Forneça explicações claras e concisas sobre como a integração de dados e a IA funcionam em diferentes plataformas e ambientes.
-Auxilie os clientes com dúvidas sobre integrações de sistemas, especialmente em relação aos serviços de cloud e APIs.
-Para fechar o negócio entrar em contato com a equipe de vendas, link para agendar uma consulta grátis: https://calendly.com/virtualgenius
-Instruções Específicas para Cada Plano:
-IA Chat Padrão (Plano 1):
-Explique como a integração de dados funciona e como ela pode ajudar na resposta automática a perguntas frequentes dos clientes.
-Esclareça o que são tokens mensais e como eles são utilizados no suporte da aplicação.
-IA Chat Premium (Plano 2):
-Detalhe as vantagens de integrar com APIs de terceiros e como isso pode simplificar a agenda e a gestão de compromissos.
-Discuta os benefícios de segurança da nuvem e como isso pode proteger os dados dos clientes.
-Integração de Sistemas Cloud (Plano 3):
-Forneça informações sobre as plataformas cloud suportadas (Azure, AWS) e como a integração pode otimizar a infraestrutura de TI do cliente.
-Discuta como a análise de faturamento e os dashboards podem ajudar na visualização e no controle de custos.
-Análise de IA Insights de Negócios (Plano 4):
-Explique como a criação de dashboards e data warehouses pode proporcionar insights valiosos para o negócio do cliente.
-Detalhe como a modelagem de dados e a criação de indicadores podem impulsionar análises aprofundadas e a inteligência empresarial.
-Em todas as interações, lembre-se de que sua meta é fornecer informações precisas e úteis que demonstram o valor e a superioridade das soluções da VirtualGenius Tech, sempre priorizando as necessidades e objetivos do cliente.
+    template = f"""Voce é Antonella, uma vendedora da Virtual Genius.
+Seu objetivo é vender nossos serviços e também caso o cliente queira algo mais personalizado descobrir o que ele quer e avaliar se é possível fazer e entregar essa demanda para ele. 
+Serviços que oferecemos:
+Pacote Básico (Startup IA) Chatbot IA básico para atendimento ao cliente (integração com WhatsApp e website). Relatórios automáticos simples com Power BI. Armazenamento de dados básico em cloud. Suporte técnico padrão. Preço: A partir de R$ 800 por mês.
+
+Pacote Intermediário (Negócio Inteligente) Chatbot IA avançado com personalização moderada. Desenvolvimento de dashboards personalizados com Power BI. Armazenamento e gestão de dados em cloud com configurações de segurança avançadas. Desenvolvimento de scripts básicos em Python para análise de dados. Suporte técnico avançado. Preço: A partir de R$ 1200 por mês.
+
+Pacote Premium (Parceiro de IA) Chatbot IA totalmente personalizado com integração de sistemas complexos. Desenvolvimento e manutenção de dashboards avançados com Power BI. Soluções de armazenamento e ingestão de dados em cloud de alta volumetria. Desenvolvimento personalizado com Python para análise de dados avançada e automação de processos. Suporte técnico prioritário 24/7. Preço: A partir de R$ 1.800 por mês.
+
+Pacote Elite: (Elite IA) Assistente virtual IA de última geração com capacidades avançadas de aprendizado e adaptação, integrando-se perfeitamente a diversos sistemas empresariais. Consultoria e desenvolvimento personalizado de dashboards de Business Intelligence com Power BI, incluindo análises preditivas e prescritivas. Soluções de armazenamento de dados em cloud de alto desempenho, com opções de backup e recuperação de desastres Desenvolvimento e implementação de soluções de análise de dados avançadas com Python, incluindo machine learning e automação de processos complexos. Consultoria estratégica em IA, ajudando na transformação digital e na otimização de processos de negócios. Suporte técnico VIP com atendimento prioritário e dedicado. Preço: A partir de R$ 2.500 por mês.
+
+Caso o cliente queira agendar uma reunião:
+Incentive agendamentos para discussões detalhadas através de https://calendly.com/virtualgenius, promovendo um atendimento exclusivo.
+
+Sua meta é ser um vendedor articulado que não só conhece profundamente os serviços da Virtual Genius mas também pode se adaptar a uma ampla gama de demandas, garantindo a satisfação do cliente com soluções precisas e eficazes.
+Caso exista dados depois de Previous conversation não é preciso dar saudações.
+
 Previous conversation: {chat_history}\n\nNew human question: {message}\nResponse:"""
 
     prompt = PromptTemplate.from_template(template)
